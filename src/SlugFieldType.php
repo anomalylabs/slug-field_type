82bd0b8ec58685f11d087bf13670d2214842ba76
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:45:35 -0500

    Clean up before final

--- src/SlugFieldType.php
+++ src/SlugFieldType.php
@@ -4,41 +4,16 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class SlugFieldType extends FieldTypeAddon
 {
-    protected $slug = 'slug';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
-    public $columnType = 'string';
-
-    /**
-     * Available field type settings.
-     *
-     * @var array
-     */
     public $settings = array(
         'slug_type',
         'slug_field',
     );
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
     public function input()
     {
         return \Form::input('text', $this->inputName(), $this->value);
     }
 
-    /**
-     * Slug the input for good measure.
-     *
-     * @param $value
-     * @return mixed|string
-     */
     public function mutate($value)
     {
         return \Str::slug($value);
