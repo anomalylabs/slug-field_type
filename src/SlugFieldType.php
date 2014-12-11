aff193006ad60226b9725f843f960c5381107463
Ryan Thompson <service@aiwebsystems.com>
2014-12-10 22:43:13 -0600

    Config syntax update

--- src/SlugFieldType.php
+++ src/SlugFieldType.php
@@ -29,7 +29,7 @@ class SlugFieldType extends FieldType
     {
         $data = parent::getInputData();
 
-        $data['separator'] = $this->getConfig('separator', '_');
+        $data['separator'] = $this->pullConfig('separator', '_');
 
         return $data;
     }
