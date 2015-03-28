725bcbc26eb303fdd69b64a9c010442403d52543
Ryan Thompson <service@aiwebsystems.com>
2015-03-27 20:34:12 -0500

    Watch

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 
-    $('input.slug').slugify({
+    var slugs = $('input.slug');
+
+    // Slugify slug inputs.
+    slugs.slugify({
         slug: 'input.slug'
+    });
+
+    slugs.each(function () {
+
+        var slug = $(this);
+        var form = $(this).closest('form');
+        var watched = form.find('[data-field="' + slug.data('watch') + '"]').first().find('input');
+
+        if (watched.length) {
+            watched.on('keyup', function () {
+                slug.val(watched.val()).trigger('keyup');
+            });
+        }
     });
 });
