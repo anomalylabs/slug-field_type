a50f791238d024fe6216234e4062ed41d05e68cf
Ryan Thompson <ryan@pyrocms.com>
2016-11-30 13:00:05 -0600

    Fix always slugify option / value switch

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
-
-    // Watch for changes if applicable.
-
+    
     $('input[data-provides="anomaly.field_type.slug"]').each(function () {
+
+        var config = {
+            slug: this,
+            lowercase: $(this).data('lowercase')
+        };
+
+        /**
+         * Only slugify other fields if
+         * value is empty OR configured
+         * to always slugify field values.
+         */
         if (!$(this).val() || $(this).data('always_slugify')) {
-
-            var config = {
-                slug: this,
-                lowercase: $(this).data('lowercase'),
-                slugify: '[data-field="' + $(this).data('slugify') + '"]:visible:first'
-            };
-
-            // Slugify slug inputs.
-            $(this).slugify(config);
+            config.slugify = '[data-field="' + $(this).data('slugify') + '"]:visible:first';
         }
+
+        // Slugify slug inputs.
+        $(this).slugify(config);
     });
 });
