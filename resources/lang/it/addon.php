7dd3504a2a3364b20ab5e0818cb3ef9454288cda
Christian Giupponi <c.giupponi@zerouno.io>
2019-10-12 19:16:47 +0200

    Add italian language

---
+++ resources/lang/it/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Slug',
+    'name'        => 'Slug Field Type',
+    'description' => 'Formatta le stringe come Slug.',
+];
---
+++ resources/lang/it/config.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'type'      => [
+        'label'        => 'Slug Type',
+        'instructions' => 'Che tipo di slug vuoi creare?',
+    ],
+    'slugify'   => [
+        'label'        => 'Slugify',
+        'instructions' => 'Inserisci il campo da trasformare automaticamente in slug.',
+    ],
+    'lowercase' => [
+        'label'        => 'Minuscolo',
+        'instructions' => 'Vuoi forzare tutti i caratteri in minuscolo?',
+    ],
+    'min'       => [
+        'label'        => 'Lunghezza minima',
+        'instructions' => 'Indica la lunghezza minima permessa.',
+    ],
+    'max'       => [
+        'label'        => 'Lunghezza massima',
+        'instructions' => 'Indica la lunghezza massima permessa.',
+    ],
+];
